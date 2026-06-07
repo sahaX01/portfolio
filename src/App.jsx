dd18a617337d4ff821ae323e97a9e0fcb8cb2323
sahaX01 <subhamsaha.email.com>
2026-06-08 01:10:54 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import Skills from "./components/Skills";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <Header />
       <Home />
       <About />
-      <Skills/>
-      <Project/>
-      <Contact/>
-      <Footer/>
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
   );
 }
